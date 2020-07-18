17bcc021bb008abcec5a6114ad048a88978e8357
Yasin Şükrü Tan <yasinsukrutan@gmail.com>
2020-07-18 18:59:16 +0300

    Array extension added for Identifiable

---
+++ Memorize/Memorize/Array+Identifiable.swift
@@ -0,0 +1,20 @@
+//
+//  Array+Identifiable.swift
+//  Memorize
+//
+//  Created by Yasin Şükrü Tan on 18.07.2020.
+//  Copyright © 2020 Yasin Şükrü Tan. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element: Identifiable {
+    func firstIndex(matching: Element) -> Int {
+        for index in 0..<self.count {
+            if self[index].id == matching.id{
+                return index
+            }
+        }
+        return 0
+    }
+}
