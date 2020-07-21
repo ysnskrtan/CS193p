b843fd9f069208d329cbfea5e0ede84620b7110a
Yasin Şükrü Tan <yasinsukrutan@gmail.com>
2020-07-21 10:49:23 +0300

    EmojiArt project created

---
+++ EmojiArt/EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Yasin Şükrü Tan on 21.07.2020.
+//  Copyright © 2020 Yasin Şükrü Tan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
